a89ccb277024aff38f74226d632a63b073fdc0fb
Omiee123 <31563932+Omiee123@users.noreply.github.com>
2019-10-14 09:45:39 +0530

    Update RSA.py

--- RSA.py
+++ RSA.py
@@ -6,16 +6,18 @@
 
 # Step 1 : n = P x Q
 # Step 2 : fi(n) = (P-1)X(Q-1)
-# Step 3 : e
+# Step 3 : e : e should lie between 1 to fn and gcd(e,fn) = 1
 # Step 4 : {e,n}
 # Step 5 : ASCII Value of M
 # Step 6 : E = (M**e)%n
 # Step 7 : Decryption : Multiplicative Inverse of e w.r.t to fi(n)
 # Step 8 : Decryption : D = e^(-1) mod fi(n)
 
-
-# In[33]:
-
+def gcd(a,b):
+    if a==0:
+        return b
+    
+    return gcd(b%a,a)
 
 def multiplicative_inverse(x, y):
     a1,b1 = 1,0
@@ -40,41 +42,24 @@ def multiplicative_inverse(x, y):
     
     return res
 
-
-# In[22]:
-
-
 # Step 1 : n = P x Q
 P = int(input("Enter prime number P : "))
 Q = int(input("Enter prime number Q : "))
 n = P*Q
 print("n : {}".format(n))
 
-
-# In[23]:
-
-
 # Step 2 : fi(n) = (P-1)X(Q-1)
 fn = (P-1)*(Q-1)
 print("fn : {}".format(fn))
 
-
-# In[24]:
-
-
 # Step 3 : e
 e = int(input("Enter number e : "))
-
-
-# In[25]:
-
+while e > fn or e < 1 or gcd(e,fn)!=1:
+    print("Condition of e is not satisfied")
+    e=int(input("Enter number e : "))
 
 # Step 4 : {e,n}
 print("pu_key = ({},{})".format(e,n))
-
-
-# In[38]:
-
 
 # Step 5 : ASCII Value of M
 M = input("Enter Message M : ")
@@ -84,10 +69,6 @@ print(M)
 ascii_array = [ ord(i) for i in M ]
 print(ascii_array)
 
-
-# In[39]:
-
-
 # Step 6 : E = (M**e)%n
 result_array = ["x"]*len(ascii_array)
 print(result_array)
@@ -96,17 +77,9 @@ for i in range(0,len(ascii_array)):
     
 print(result_array)
 
-
-# In[40]:
-
-
 # Step 7 : Decryption : Multiplicative Inverse of e w.r.t to fi(n)
 e1 = multiplicative_inverse(13, 352)
-print(e1)
-
-
-# In[41]:
-
+print("Private Key : ({},{})".format(e1,n))
 
 # Step 8 : Decryption : D = c1**e^(-1) mod fi(n)
 output_array = ["x"]*len(ascii_array)
@@ -116,10 +89,6 @@ for i in range(0,len(ascii_array)):
     
 print(output_array)
 
-
-# In[42]:
-
-
 # Step 9 : Resultant String
 result = ""
 
@@ -127,10 +96,3 @@ for i in range(0,len(ascii_array)):
     result += chr(output_array[i])
     
 print(result)
-
-
-# In[ ]:
-
-
-
-
