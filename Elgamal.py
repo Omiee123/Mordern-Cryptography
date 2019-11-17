aabedde2d6f94f0e983d16de6d0960ed021e1b5a
Omiee123 <31563932+Omiee123@users.noreply.github.com>
2019-11-17 22:21:43 +0530

    Update Elgamal.py

--- Elgamal.py
+++ Elgamal.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[47]:
-
-
 print("Welcome Alice :")
 print("To Contact with Bob Generate Key")
 p = int(input("Enter prime number p : "))
@@ -24,21 +21,12 @@ M = int(input("Enter Message : "))
 b = (M*(Y**k))%p
 print("b : "+str(b))
 
-
-# In[48]:
-
-
 #Step 2 - Receiver Decrypt Cipher Text With It's Own Private Key
 Plain = ((a**(p-x-1))*b)%p
 print("Original Message : "+str(Plain))
-
-
-# In[ ]:
-
 
 #Chek following example
 #(P=71, G=33, x=62, M=15, k=31)
 #(P=23, G=11, x=6, M=10, k=3)
 #(P=11, G=2, x=8, M=5, k=9)
 #(P=1697, G=138, x=164, M=19, k=159)
-
